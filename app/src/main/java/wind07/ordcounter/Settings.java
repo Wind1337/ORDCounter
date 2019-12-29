539f5c7af89df20b476bc6baa0369f1c48bf2a35
Carlsen Teo <carlsen@inspirous.us>
2019-12-29 17:19:23 +0800

    Fallback to java
    Because I suck
    
    Signed-off-by: Carlsen Teo <carlsen@inspirous.us>

---
+++ app/src/main/java/wind07/ordcounter/Settings.java
@@ -0,0 +1,18 @@
+package wind07.ordcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_settings, new SettingsFragment())
+                .commit();
+    }
+}
---
+++ app/src/main/java/wind07/ordcounter/SettingsFragment.java
@@ -0,0 +1,13 @@
+package wind07.ordcounter;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.app_preferences, rootKey);
+    }
+}
