3fb1aec0384333116b90daaebe3faec087b15cfc
Carlsen Teo <carlsen@inspirous.us>
2020-06-06 20:34:55 +0800

    Comment out unused functions and layouts
    Signed-off-by: Carlsen Teo <carlsen@inspirous.us>

--- app/src/main/java/wind07/ordcounter/MainActivity.kt
+++ app/src/main/java/wind07/ordcounter/MainActivity.kt
@@ -31,10 +31,10 @@ class MainActivity : AppCompatActivity() {
         calOrdDays()
         calProgress()
 
-        fab.setOnClickListener { view ->
+        /* fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
-        }
+        } */
     }
 
     override fun onResume() {
