627600b1e3ad80a7aea871bd5e4d885041bd4d96
Carlsen Teo <carlsen@inspirous.us>
2020-07-04 16:18:15 +0800

    Settings: Fix toolbar not showing activity name
    Signed-off-by: Carlsen Teo <carlsen@inspirous.us>

--- app/src/main/java/wind07/ordcounter/Settings.java
+++ app/src/main/java/wind07/ordcounter/Settings.java
@@ -3,6 +3,7 @@ package wind07.ordcounter;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class Settings extends AppCompatActivity {
 
@@ -10,6 +11,9 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.action_settings));
+        setSupportActionBar(toolbar);
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.fragment_settings, new SettingsFragment())
